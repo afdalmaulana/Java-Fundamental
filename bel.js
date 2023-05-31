39e51abb1b335a8bbb6da33d8d914e15532324c3
Muh. Afdal Maulana Said <68625050+afdalmaulana@users.noreply.github.com>
2023-05-31 22:42:42 +0700

    Delete bel.js

--- bel.js
+++
@@ -1,8 +0,0 @@
-let array5 = ["A", "B", "C", "D"];
-console.log (array5.push("F"));
-let tampung = [];
-tampung.push(array5[2]); // memasukkan isi data array "C" ke variable tampung
-console.log (array5);
-console.log (tampung);
-// array5.push("G"); 
-// console.log (array5);
